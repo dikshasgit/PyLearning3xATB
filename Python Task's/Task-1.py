d6b0fd4443b352034611321f0fbb95234fc5763d
diksha yadav <dikshayadav625@gmail.com>
2024-06-12 14:36:59 +0530

    Literals

---
+++ Python Task's/Task-1.py
@@ -0,0 +1,51 @@
+#Task-1
+# 1.Explain the difference between the = operator and the == operator in Python.
+# 2.What does the ** operator do in Python, and how is it used?
+# 3.What does the ^ operator do in Python, and in what context is it commonly used?
+
+#a. == - is a camparison operator ,used it to campare 2 values
+
+x = 5
+y = 3
+print(x == y)
+
+#b = - is a assignment operator , are used to assign values to variables
+
+x = 5
+print(x)
+
+#c ** - is a arthemetic operator called exponential operator ,
+#       used with numeric value to perform common mathmatical operations
+
+x = 2
+y = 5
+print(x ** y)    # same as 2*2*2*2*2
+
+#d ^ - is bitwise operator called XOR, are used to compare binary numbers
+       # set each bit to 1 if only one of two bits is 1
+
+print(6^3)
+"""
+The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to
+ 0:
+
+6 = 0000000000000110
+3 = 0000000000000011
+--------------------
+5 = 0000000000000101
+====================
+
+Decimal numbers and their binary values:
+0 = 0000000000000000
+1 = 0000000000000001
+2 = 0000000000000010
+3 = 0000000000000011
+4 = 0000000000000100
+5 = 0000000000000101
+6 = 0000000000000110
+7 = 0000000000000111
+"""
+
+
+
+
---
+++ Python Task's/Task-2.py
@@ -0,0 +1,40 @@
+# Task #2
+
+# 1. Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+# 2. Create a program that takes two numbers as input and prints whether the first number is greater than, less than,
+# or equal to the second number.
+
+# 2.
+num1 = 5
+num2 = 6
+
+
+def compare_numbers(num1, num2):
+    if num1 > num2:
+        return "First no is greater than second no."
+    elif num1 < num2:
+        return "First no. is less than second no."
+    else:
+        return "First no is equal to second"
+
+
+num1 = float(input("Enter the first no"))
+
+num2 = float(input("Enter the second no."))
+result = compare_numbers(num1, num2)
+print(result)
+print(input(num1))
+
+
+#1.
+def calculate_circle_area(radius):
+    pi = 3.14
+    area = pi*(radius ** 2)
+    return area
+radius = float(input("Enter the radius of circle:"))
+area = calculate_circle_area(radius)
+print(f"The area of the circle with radius{radius} is {area}")
+
+
+
+
